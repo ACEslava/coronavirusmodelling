29cd4d0efdf74da8fb23e0f4b52abdc77a3424ee
Aaron Eslava <aeslava1970@gmail.com>
2020-07-22 21:02:36 -0700

    See README for Changelog

--- SEIRAHD.m
+++ SEIRAHD.m
@@ -1,13 +1,21 @@
 clear all
 close all
 clc
-% Parameters
-%load FB2404
-%A=ones(2404)-eye(2404);
+
+%Parameters
+alpha = 0.09; 
+beta = 0.1;
+gamma = 0.08; 
+delta = 0.7; 
+theta = 0.4;
+h = 0.2033;
+omega = 0.1;
+Seeds = 1;
+TestingError = 0.7; %Percent of tests that are true positive/true negative.
+movement = 1; %Initial movement is 1*normal movement
+Asiz = 5000;
 
 %Creates the initial graph
-Asiz = 5000;
-movement = 1; %Initial movement is 1*normal movement
 positionx = unifrnd(-1,1,Asiz,1);
 positiony = unifrnd(-1,1,Asiz,1);
 node1=positionx;
@@ -25,16 +33,6 @@ for j=1:Asiz
         A(i,j) = Distance(i,j) < proximity;
     end
 end
-
-%Parameters
-alpha = 0.09; 
-beta = 0.1;
-gamma = 0.08; 
-delta = 0.7; 
-theta = 0.99999820042;
-h = 0.2033;
-omega = 0.01;
-Seeds = 1;
 
 %Initialise variables
 E = zeros(Asiz,1); %assigns initial seeds
@@ -62,12 +60,13 @@ while (sum(E) + sum(As) + sum(I) > 0)
     node1=positionx+movement*(unifrnd(-0.1,0.1,Asiz,1));
     node2=positiony+movement*(unifrnd(-0.1,0.1,Asiz,1));
     
+    %Checks if nodes are out of bounds
     node1(node1>1) = -1;
     node1(node1<-1) = 1;
     node2(node2>1) = -1;
     node2(node2<-1) = 1;
     
-
+    %Regenerates the graph based on movements
     for j=1:Asiz
         for i=1:Asiz
             Distance(i,j) = sqrt((node1(i)-node1(j))^2+(node2(i)-node2(j))^2);
@@ -79,7 +78,7 @@ while (sum(E) + sum(As) + sum(I) > 0)
     
     %Mask wearers
     Masks = zeros(Asiz,1);
-    for maskcount=1:floor(Asiz*0.5) %mask wearing percentage
+    for maskcount=1:floor(Asiz*0.5)
         Masks(ceil(Asiz*rand)) = 1; %assigns random people to be mask wearers according to percentage defined in for loop
     end
 
@@ -92,6 +91,7 @@ while (sum(E) + sum(As) + sum(I) > 0)
     SumR(t) = sum(R);
     SumD(t) = sum(D);
 
+    %Counts days where no infected individuals exist
     if SumI(t) == 0
         zerodays = zerodays+1;
     else
@@ -99,8 +99,8 @@ while (sum(E) + sum(As) + sum(I) > 0)
     end
     
     %Lockdown Strategy
-    if (SumI(t)/Asiz) > 0.005 %Full Lockdown
-        proximity = 0.03;
+    if ((TestingError*SumI(t))/Asiz) > 0.005 %Full Lockdown
+        proximity = 0.06;
         movement = 0.4;
         lockdown(t) = 1;
     elseif zerodays > 14 %None
@@ -114,13 +114,14 @@ while (sum(E) + sum(As) + sum(I) > 0)
         lockdown(t) = lockdown(t-1);
     end
     
-    if lockdowndays > 30 %Restricted Lockdown
-        proximity = 0.05; 
-        movement = 0.8;
+    if lockdowndays > 60 %Restricted Lockdown
+        proximity = 0.07; 
+        movement = 0.55;
         lockdown(t) = 0.5;
         lockdowndays = 0;
     end
     
+    %Updates Lockdown Counters
     if lockdown(t) == 1
         FullLockdown = FullLockdown + 1;
         lockdowndays = lockdowndays + 1;
@@ -146,6 +147,7 @@ while (sum(E) + sum(As) + sum(I) > 0)
     INeighbors_Mask = A*(or(I,As)).*Masks;
     INeighbors_NMask = INeighbors - INeighbors_Mask;
     
+    %Infecting Susceptible
     if mask(t) ==1
         NewE=rand(Asiz,1)<1-(1-(1-0.3*Masks)*beta).^INeighbors_NMask.*(1-(0.05-0.035*Masks)*beta).^INeighbors_Mask; %infected given that at you/neighbor wore mask
     else
@@ -180,15 +182,11 @@ while (sum(E) + sum(As) + sum(I) > 0)
     
     %H to D
     anotherrand = rand(Asiz, 1);
-    age85 = and ((anotherrand < 0.03), and ((HRandom < 0.02), boolean(H)));
-    age75 = and ((anotherrand < 0.02), and (and ((HRandom > 0.02), (HRandom < 0.06)), boolean(H)));
-    age65 = and ((anotherrand < 0.01), and (and ((HRandom > 0.06), (HRandom < 0.16)), boolean(H)));
-    ageother = and ((anotherrand < 0.001), and ((HRandom > 0.16), boolean(H)));
-    
-    NewD = or (age85, age75);
-    NewD = or (NewD, age65);
-    NewD = or (NewD, ageother);
-    NewD = and (not(NewR), NewD);
+    age85 = and ((anotherrand < 0.1), and ((HRandom < 0.02), boolean(H)));
+    age75 = and ((anotherrand < 0.06), and (and ((HRandom > 0.02), (HRandom < 0.06)), boolean(H)));
+    age65 = and ((anotherrand < 0.04), and (and ((HRandom > 0.06), (HRandom < 0.16)), boolean(H)));
+    ageother = and ((anotherrand < 0.01), and ((HRandom > 0.16), boolean(H)));
+    NewD = and (not(NewR), or (or (or (age85, age75), age65), ageother));
 
     %Update indicators
     S = S - NewE;
@@ -198,19 +196,22 @@ while (sum(E) + sum(As) + sum(I) > 0)
     H = H + NewH - (HtoR + NewD);
     R = R + NewR;
     D = D + NewD;
-    t
-    t=t+1;
+    t=t+1
 end
-SumS(t:1000) = [];
-SumE(t:1000) = [];
-SumI(t:1000) = [];
-SumR(t:1000) = [];
-SumA(t:1000) = [];
-SumH(t:1000) = [];
-SumD(t:1000) = [];
+% SumS(t:1000) = [];
+% SumE(t:1000) = [];
+% SumI(t:1000) = [];
+% SumR(t:1000) = [];
+% SumA(t:1000) = [];
+% SumH(t:1000) = [];
+% SumD(t:1000) = [];
+
+if t < 200
+    lockdown(t:200) = [];
+    mask(t:200) = []
+end
 
 TotalDeaths = SumD(t-1)
-DaysLockdown = sum(lockdown)
 DaysCOVID = t
 FullLockdown
 RestrictedLockdown
@@ -225,21 +226,32 @@ semilogy(SumA, 'Color', '#ff7f00', 'LineWidth',1.5, 'DisplayName','Asymptomatic'
 semilogy(SumH, 'Color', '#984ea3', 'LineWidth',1.5, 'DisplayName','Hospitalised');
 semilogy(SumR, 'Color', '#4daf4a', 'LineWidth',1.5, 'DisplayName','Recovered');
 semilogy(SumD, 'k','LineWidth',1.5,'DisplayName','Deceased');
+L = area(lockdown.*Asiz, 'LineStyle', ':', 'DisplayName', 'Lockdown');
+L.FaceColor = [0.1 0.1 0.5];
+L.FaceAlpha = 0.1;
+
+M = area(mask.*Asiz, 'LineStyle', ':', 'DisplayName', 'Masks');
+M.FaceColor = [0.5 0.1 0.1];
+M.FaceAlpha = 0.1;
+xlim([0 t-1]);
+grid on;
 
 figure(2);
-plot(lockdown, 'Displayname', 'Lockdown')
-ylim([0 1.1])
-xlim([0 t+5])
+plot(SumS, 'Color', '#377eb8', 'LineWidth',1.5, 'DisplayName','Susceptible');
+plot(SumE, 'Color', '#62466B', 'LineWidth',1.5, 'DisplayName','Exposed');
+plot(SumI, 'Color', '#e41a1c', 'LineWidth',1.5, 'DisplayName','Infected');
+plot(SumA, 'Color', '#ff7f00', 'LineWidth',1.5, 'DisplayName','Asymptomatic');
+plot(SumH, 'Color', '#984ea3', 'LineWidth',1.5, 'DisplayName','Hospitalised');
+plot(SumR, 'Color', '#4daf4a', 'LineWidth',1.5, 'DisplayName','Recovered');
+plot(SumD, 'k','LineWidth',1.5,'DisplayName','Deceased');
 
-figure(3);
-plot(mask, 'Displayname', 'Mask')
-ylim([0 1.1])
-xlim([0 t+5])
+L = area(lockdown.*Asiz, 'LineStyle', ':','DisplayName', 'Lockdown'); hold on
+L.FaceColor = [0.1 0.1 0.5];
+L.FaceAlpha = 0.1;
 
-% figure(2);
-% plot(lockdown, 'Displayname', 'Lockdown');hold on
-% plot(mask, 'Displayname', 'Mask')
-% ylim([0 1.1])
-% xlim([0 t+5])
+M = area(mask.*Asiz, 'LineStyle', ':', 'DisplayName', 'Masks');
+M.FaceColor = [0.5 0.1 0.1];
+M.FaceAlpha = 0.1;
+xlim([0 t-1]);
+grid on;
 
-grid on
